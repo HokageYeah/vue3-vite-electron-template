bae5b083cb4cf6e2dee55348b78b708f2bbf310d
yuye <18838280615@163.com>
2024-07-06 08:53:22 +0800

    发哦lf空格 mac

--- plugins/vite.electron.dev.ts
+++ plugins/vite.electron.dev.ts
@@ -1,12 +1,13 @@
 // electron 开发环境的插件
-import type { AddressInfo } from 'net'
-import type { Plugin } from 'vite'
-import { spawn } from 'node:child_process'
-import fs from 'node:fs'
+import type { AddressInfo } from 'node:net';
+import { spawn } from 'node:child_process';
+import fs from 'node:fs';
+import type { Plugin } from 'vite';
 
 // 打包代码单独封装
 const buildBackground = () => {
   // 将background.ts文件编译成js文件, 因为electron不认识ts文件
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   require('esbuild').buildSync({
     entryPoints: ['src/background.ts'],
     bundle: true,
@@ -14,8 +15,8 @@ const buildBackground = () => {
     platform: 'node',
     target: 'node12',
     external: ['electron']
-  })
-}
+  });
+};
 
 // vite  插件要求必须要导出一个对象，对象必须有name属性
 export const ElectronDevPlugin = (): Plugin => {
@@ -23,39 +24,40 @@ export const ElectronDevPlugin = (): Plugin => {
     name: 'electron-dev',
     configureServer(server) {
       // 将background.ts文件编译成js文件
-      buildBackground()
+      buildBackground();
       server.httpServer?.once('listening', () => {
         // 读取vite服务的信息
-        const addressInfo = server.httpServer?.address() as AddressInfo
+        const addressInfo = server.httpServer?.address() as AddressInfo;
         // 拼接给electron启动服务的时候要用的ip地址
-        const IP = `http://localhost:${addressInfo.port}`
-        console.log('哈哈哈---------', IP)
+        const IP = `http://localhost:${addressInfo.port}`;
+        console.log('哈哈哈---------', IP);
         // 第一个参数是electron的入口文件
         // require('electron')返回是一个路径
         // electron 是不认识ts文件，需要先进行编译成js文件
         // 进程传参法，发送给electron IP地址
-        const electronStr = require('electron') as unknown as String
-        console.log('electron的入口文件---------', electronStr)
-        let electronProcess = spawn(<string>electronStr, ['dist/background.js', IP])
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const electronStr = require('electron') as unknown as String;
+        console.log('electron的入口文件---------', electronStr);
+        let electronProcess = spawn(<string>electronStr, ['dist/background.js', IP]);
 
         // 监听src下面的background.ts文件，只要改变就先杀死进程在重新启动electron，实现热更新
         fs.watchFile('src/background.ts', () => {
           // 先杀死electron进程
-          electronProcess.kill()
+          electronProcess.kill();
           // 将background.ts文件编译成js文件
-          buildBackground()
-          electronProcess = spawn(<string>electronStr, ['dist/background.js', IP])
-        })
+          buildBackground();
+          electronProcess = spawn(<string>electronStr, ['dist/background.js', IP]);
+        });
         // 监听electron进程输出的日志
         electronProcess.stderr.on('data', (data) => {
-          console.log('data日志输出-----', data)
-        })
+          console.log('data日志输出-----', data);
+        });
         // 关闭窗口，主进程和子进程也会跟着退出
         electronProcess.on('close', () => {
-          server.close()
-          process.exit()
-        })
-      })
+          server.close();
+          process.exit();
+        });
+      });
     }
-  }
-}
+  };
+};
