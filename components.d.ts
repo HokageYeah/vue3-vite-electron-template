70942b70a9b465062d018b4768e2a7c26284cd15
yuye <659672626@qq.com>
2024-07-07 23:13:39 +0800

    添加elementpuls UI 并重写模板首页样式

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,14 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    ElAside: typeof import('element-plus/es')['ElAside']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElIcon: typeof import('element-plus/es')['ElIcon']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    ElMenu: typeof import('element-plus/es')['ElMenu']
+    ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
+    ElScrollbar: typeof import('element-plus/es')['ElScrollbar']
+    ElSubMenu: typeof import('element-plus/es')['ElSubMenu']
     HomeTest: typeof import('./src/components/HomeTest.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,118 @@
+<template>
+  <el-container class="container">
+    <el-aside class="container-left">
+      <div class="sidebar-logo-container">
+        <img src="/favicon.png" class="sidebar-logo" />
+        <h1 class="sidebar-title">桌面程序</h1>
+      </div>
+      <div class="has-logo">
+        <el-scrollbar wrap-class="scrollbar-wrapper">
+          <el-menu
+            :unique-opened="true"
+            active-text-color="#ffd04b"
+            background-color="#545c64"
+            text-color="#fff"
+            default-active="/home"
+            :default-openeds="['/home']"
+            class="el-menu-vertical-demo"
+            @open="handleOpen"
+            @close="handleClose"
+          >
+            <el-sub-menu index="1">
+              <template #title>
+                <el-icon><Notebook /></el-icon>
+                <span>Home主页一级菜单</span>
+              </template>
+              <RouterLink to="/home">
+                <el-menu-item index="/home">Home主页二级</el-menu-item>
+              </RouterLink>
+            </el-sub-menu>
+          </el-menu>
+        </el-scrollbar>
+      </div>
+    </el-aside>
+    <el-main class="container-right"><RouterView /> </el-main>
+  </el-container>
+</template>
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 </script>
 
-<template>
-  <header>
-    <RouterLink to="/">Home</RouterLink>
-  </header>
-
-  <RouterView />
-</template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style scoped lang="scss">
+.tac {
+  & .has-logo {
+    height: 1000px;
+    background-color: #304156;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  & .sidebar-logo-container {
+    overflow: hidden;
+    position: relative;
+    width: 100%;
+    height: 50px;
+    background: #2b2f3a;
+    line-height: 50px;
+    text-align: center;
+    & .sidebar-logo {
+      margin-right: 12px;
+      width: 32px;
+      height: 32px;
+      vertical-align: middle;
+    }
+    & .sidebar-title {
+      display: inline-block;
+      margin: 0;
+      background: #2b2f3a;
+      line-height: 50px;
+      vertical-align: middle;
+      font-weight: 600;
+      font-size: 18px;
+      color: #fff;
+    }
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+}
+.container {
+  &-left {
+    width: 210px;
+    & .has-logo {
+      position: fixed;
+      left: 0;
+      top: 50px;
+      bottom: 0;
+      width: 210px;
+      background-color: #304156;
+      .scrollbar-wrapper .el-menu {
+        border-right: 0 solid red;
+      }
+    }
+    & .sidebar-logo-container {
+      overflow: hidden;
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      width: 210px;
+      height: 50px;
+      background: #2b2f3a;
+      line-height: 50px;
+      text-align: center;
+      & .sidebar-logo {
+        margin-right: 12px;
+        width: 32px;
+        height: 32px;
+        vertical-align: middle;
+      }
+      & .sidebar-title {
+        display: inline-block;
+        margin: 0;
+        background: #2b2f3a;
+        line-height: 50px;
+        vertical-align: middle;
+        font-weight: 600;
+        font-size: 18px;
+        color: #fff;
+      }
+    }
   }
 }
 </style>
